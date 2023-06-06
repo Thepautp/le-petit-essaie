03490f093f08c8cf897d29a8cb60035dca4c8b5f
p-thepaut <p-thepaut@ankh-systems.co.jp>
2023-06-06 17:58:34 +0900

    add get profile

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
         liffId: "1661282389-E5XPXoVd",
     }).then(() => {
         displayMessage("LIFF initialized");
-    }).catch ((error) => {
+        liff.getProfile().then((profile) => {
+            onLogin(profile.avatarUr, profile.name, profile.status);
+        }).catch ((error) => {
+            displayMessage(error);
+        });
+    }).catch((error) => {
         displayMessage(error);
     });
 });
@@ -11,4 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
 function displayMessage(message) {
     var messageBanner = document.getElementById('error');
     messageBanner.innerText = message;
+}
+
+function onLogin(avatarUr, name, status) {
+    const avatarElement = document.querySelector('.profile__avatar');
+    const nameElement = document.querySelector('.profile__name');
+    const statusElement = document.querySelector('.profile__status');
+
+    avatarElement.src = 'https://placekitten.com/g/200/300';
+    nameElement.textContent = 'John Doe';
+    statusElement.textContent = 'Online';
 }
